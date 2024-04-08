ac09e121cba768b6d3756755a3928408fd7c8730
Pedro <pedrogabrielcruzlima@gmail.com>
2024-04-08 15:16:45 -0300

    adicionando gerenciador de caminho

---
+++ FormatadorDeArquivoDePagamento/GerenciadorDeCaminho.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatadorDeArquivoDePagamento
+{
+    public class GerenciadorDeCaminho
+    {
+        private static bool CaminhoValido(string caminho)
+        {
+            if (string.IsNullOrEmpty(caminho))
+            {
+                Console.WriteLine("Caminho vazio. Tente novamente.");
+                return false;
+            }
+
+            if (!File.Exists(caminho))
+            {
+                Console.WriteLine("Arquivo não encontrado. Tente novamente.");
+                return false;
+            }
+                
+
+            return true;
+        }
+
+        public static string ObterCaminho()
+        {
+            string caminho = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Informe o caminho do arquivo:");
+                caminho = Console.ReadLine();
+            } 
+            
+            while (!CaminhoValido(caminho));
+
+            return caminho;
+        }
+    }
+}
--- FormatadorDeArquivoDePagamento/Program.cs
+++ FormatadorDeArquivoDePagamento/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FormatadorDeArquivoDePagamento;
+
+Console.WriteLine("Bem vindo ao Formatador de Arquivo de Pagamento!");
+
+string caminho = GerenciadorDeCaminho.ObterCaminho();
+
